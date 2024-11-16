0b96a698f7b3e013dc71d81fb1e24e85cd8dd954
Anndy-O <napssters@gmail.com>
2024-11-16 11:38:41 -0500

    landing

--- src/app/modules/home/projects/projects.component.ts
+++ src/app/modules/home/projects/projects.component.ts
@@ -4,13 +4,13 @@ import projectsData from '../../../../assets/jsons/projects.json';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css'],
+  styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  projects: any[] = [];
-  currentIndex: number = 0;
 
-  constructor() {}
+  projects: any[] = [];
+
+  constructor() { }
 
   ngOnInit() {
     this.loadProjects();
@@ -20,19 +20,4 @@ export class ProjectsComponent implements OnInit {
     this.projects = projectsData.cards;
   }
 
-  moveLeft() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.projects.length - 1; // Vuelve al último si está al principio
-    }
-  }
-
-  moveRight() {
-    if (this.currentIndex < this.projects.length - 1) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0; // Vuelve al principio si está al final
-    }
-  }
 }
